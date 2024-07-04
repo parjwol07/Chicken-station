557f0154892037ae1160e35c240f87f2c844d37a
parjwol07 <thapaparjwol2@gmail.com>
2024-07-04 06:26:37 +0530

    Made change .js

--- first.js
+++ first.js
@@ -1,1 +1,21 @@
-// alert("Welcome to Chicken Station");
+const signIn = document.querySelector(".signin");
+const button = document.querySelector("button");
+const btn1 = document.querySelector(".btn1");
+const body = document.querySelector("body")
+console.log(signIn,button,btn1);
+button.onclick=()=>{
+  signIn.innerText = "You have been Log In!Click Home Again";
+  signIn.style.fontSize="20px";
+  signIn.style.display="flex";
+  signIn.style. justifyContent="center";
+  signIn.style.alignItems="center";
+}
+//These for change to dark mode
+// btn1.onclick=()=>{
+//     body.style.backgroundColor="black";
+//     body.style.Color="white";
+    
+// }
+// btn1.ondblclick=()=>{
+//     body.style.backgroundColor="#fdf0d5";    
+// }
